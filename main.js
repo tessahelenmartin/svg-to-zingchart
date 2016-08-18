93b0b6c4e744c018efeada403c4ee1d832b59ba3
tessahm <tessahm@umich.edu>
2016-08-18 08:56:13 -0700

    Changes to layout

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 var pokescript = require('./pokemon-scripts');
 var SVGSTRING;
-window.addEventListener("load", beginApp);
-function beginApp() {
-    document.getElementById('select_Region').addEventListener("change", function () {
-        if (document.getElementById('select_Region').value != "")
-        {
-            var location = new XMLHttpRequest();
-            var url = '/region_maps/' + document.getElementById('select_Region').value + '.svg';
-            location.onreadystatechange = function() {
-                if (location.readyState == 4 && location.status == 200) {
-                    SVGSTRING = location.response;
-                    loadSVG()
-                }
-            };
-            location.open("GET", url, true);
-            location.send();
+window.addEventListener('load',function () {
+    pokescript.setUp(6);
+    beginApp('Kalos')
+})
+function beginApp(region_in) {
+    if (document.getElementById('subtext_region').text != "")
+    {
+        var location = new XMLHttpRequest();
+        var url = '/region_maps/' + region_in.toLowerCase() + '.svg';
+        location.onreadystatechange = function() {
+            if (location.readyState == 4 && location.status == 200) {
+                SVGSTRING = location.response;
+                loadSVG()
+            }
+        };
+        location.open("GET", url, true);
+        location.send();
 
-        }
-    }, false);
+    }
     function loadSVG() {
         var parser = new DOMParser();
         var SVGObject = parser.parseFromString(SVGSTRING, "image/svg+xml");
@@ -25,3 +26,29 @@ function beginApp() {
     }
 }
 
+var region_array = ['Kanto', 'Johto', 'Hoenn', 'Sinnoh', 'Unova','Kalos'];
+
+var map_iterator = 5;  // the index of the current item to show
+
+document.getElementById("back_region").onclick = function () {
+    map_iterator+=5;
+    map_iterator%=6;
+    document.getElementById('subtext_region').innerHTML = region_array[map_iterator];
+    pokescript.setUp(document.getElementById('subtext_region').text);
+    beginApp(region_array[map_iterator])
+}
+document.getElementById("next_region").onclick = function () {
+    map_iterator++;
+    map_iterator%=6;
+    document.getElementById('subtext_region').innerHTML = region_array[map_iterator];
+    pokescript.setUp(document.getElementById('subtext_region').text);
+    beginApp(region_array[map_iterator])
+}
+
+
+// setInterval(function() {            // setInterval makes it run repeatedly
+//     document
+//         .getElementById('HEADER_subtext')
+//         .innerHTML = title[i++];    // get the item and increment
+//     if (i == title.length) i = 0;   // reset to first element if you've reached the end
+// }, 1000);
